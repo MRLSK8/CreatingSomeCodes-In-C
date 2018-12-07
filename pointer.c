e37ce91d2543ec957e4dc9d2068306582b1170c2
José Marcelo de Lima <marcelo_sp5@hotmail.com>
2018-12-07 16:21:39 -0300

    Update pointer.c

--- pointer.c
+++ pointer.c
@@ -7,16 +7,16 @@ int main(void){
 	l = &k;
 
 	printf("\n &i = %p\n", &i);
-  printf("\n &j = %p\n", &j);
+        printf("\n &j = %p\n", &j);
 	printf("\n &k = %p\n", &k);
 	printf("\n &l = %p\n", &l);
 
 	printf("\n i = %d\n", i);  // i = 3 
-  printf("\n j = %p\n", j);  // j = &i
+        printf("\n j = %p\n", j);  // j = &i
 	printf("\n k = %p\n", k);  // k = &j
 	printf("\n l = %p\n", l);  // l = &k
 
-  printf("\n *j = %d\n", *j);     // *j = 3
+        printf("\n *j = %d\n", *j);     // *j = 3
 	printf("\n *k = %p\n", *k);     // *k = &i
 	printf("\n **k = %d\n", **k);   // **k = 3
 	printf("\n *l = %p\n", *l);     // *l = &j
